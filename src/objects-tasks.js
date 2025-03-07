b450a44c1bc00f56c4c99d6668dce2fd5f861a69
Enver <go_enver1@mail.ru>
2025-03-07 16:21:41 +0300

    feat: add mergeObjects task solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+  objects.forEach((obj) =>
+    Object.keys(obj).forEach((k) => {
+      if (res[k]) {
+        res[k] += obj[k];
+      } else {
+        res[k] = obj[k];
+      }
+    })
+  );
+  return res;
 }
 
 /**
