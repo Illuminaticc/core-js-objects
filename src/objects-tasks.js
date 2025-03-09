62e1137d5881683ddba4981b6e7351d544869388
Enver <go_enver1@mail.ru>
2025-03-09 20:02:22 +0300

    fix: isEmptyObject task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -95,7 +95,7 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({a: 1}) => false
  */
 function isEmptyObject(obj) {
-  return Object.keys(obj).length > 0;
+  return Object.keys(obj).length === 0;
 }
 
 /**
